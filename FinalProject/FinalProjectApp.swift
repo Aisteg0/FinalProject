4c569104cced5c33cd94a22dd1e33e33682d002d
Misha <Mishka.ganin@mail.ru>
2024-08-09 18:00:09 +0300

    init

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Михаил Ганин on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
