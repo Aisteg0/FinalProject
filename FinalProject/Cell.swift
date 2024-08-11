66839f7c60d9f555a7c7a4ecd878aea7ccb716fe
Misha <Mishka.ganin@mail.ru>
2024-08-11 21:55:28 +0300

    add cell

--- FinalProject/Cell.swift
+++ FinalProject/Cell.swift
@@ -19,47 +19,43 @@ struct Cell: View {
                 .frame(width: 56, height: 56)
                 .clipShape(Circle())
                 .padding(.leading, 24)
+            
             VStack(alignment: .leading) {
                 Text(dataItem.name)
                     .font(.headline)
-                if let unread = dataItem.lastMessage?.previewText, let coutUnreadMessage = dataItem.unreadMessages {
-                    HStack {
-                        Text("\(unread)")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                        
-                        Spacer()
-                        
-                        VStack {
-                            Text("17.05")
-                            //                                .frame(width: 16, height: 16)
-                            //                                .padding(.trailing, 24)
-                            
-                            Circle()
-                                .fill(colorScheme == .light ? Color.colorForCirleLigth : Color.colorForCirleDark)
-                                .frame(width: 21, height: 20)
-                                .overlay(
-                                    Text("\(coutUnreadMessage)")
-                                        .font(.caption)
-                                        .foregroundColor(Color.black)
-                                    
-                                )
-//                                .padding(.trailing, 24)
-                        }
-                        .frame(width: 55)
-                    }
-                    
+                
+                if let unread = dataItem.lastMessage?.previewText {
+                    Text("\(unread)")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
                 }
             }
-            .padding(.vertical, 8)
-            .frame(maxWidth: .infinity, alignment: .leading)
-//            .padding()
-            .frame(width: 327, height: 56)
-            .cornerRadius(8)
-            .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+            
+            Spacer()
+            
+            VStack {
+                if  let coutUnreadMessage = dataItem.unreadMessages  {
+                    Text("17.05")
+                    Circle()
+                        .fill(colorScheme == .light ? Color.colorForCirleLigth : Color.colorForCirleDark)
+                        .frame(width: 21, height: 20)
+                        .overlay(
+                            Text("\(coutUnreadMessage)")
+                                .font(.caption)
+                                .foregroundColor(Color.black)
+                        )
+                }
+            }
+            .frame(width: 55)
         }
+        .padding(.vertical, 8)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .frame(width: 327, height: 56)
+        .cornerRadius(8)
+        .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
     }
 }
+
 
 // MARK: Перенести в Extetshion+Color
 
